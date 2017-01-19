52944cb8b09b9bf5833c412733713ee897eec879
Thami Benjelloun <thami.benjelloun@ensimag.grenoble-inp.fr>
2017-01-19 11:33:10 +0100

    multiplicatio grand float

--- src/main/resources/include/GrandFloat.java
+++ src/main/resources/include/GrandFloat.java
@@ -18,26 +18,54 @@ public class GrandFloat {
     public String toString(){
         return " grand float =" + this.f +" + "+ this.erreur ;
     }
-    public static GrandFloat additionSimple(float f, float g){
-        // cette fonction a un sens si g est au moins deux fois plus petit que  f
-        float somme = f+g;
-        //on récupère l'erreure commise si les nombres n'ont pas même exposant
-        float erreur = g -(somme - f);
-        GrandFloat r= new GrandFloat(somme,erreur);
-        return r;
-    }
-    public static GrandFloat additionPrecise(float f, float g){
-        float somme= f+g;
-        float tmp = somme-f;
-        float erreur= (f-(somme-tmp)) + (g-tmp);
-        GrandFloat r= new GrandFloat(somme,erreur);
-        return r;
 
+
+
+    // Somme deux float et garde l'erreur dans notre structure
+    public static GrandFloat additionSimple(float f, float g){
+        float somme = f+g;
+        // on récupère le f erroné
+        float fprime = somme -g;
+        // on récupère le g erroné
+        float gprime = somme -fprime;
+        //on calcul l'erreur sur f
+        float erreur_f = f- fprime;
+        // on calcul l'erreur sur g
+        float erreur_g = g-gprime;
+        //on somme les erreurs
+        float erreur = erreur_f + erreur_g;
+        GrandFloat r= new GrandFloat(somme,erreur);
+        return r;
     }
+
+
+
+
+
+
+
+
+    //public static GrandFloat additionPrecise(float f, float g){
+      //  float somme= f+g;
+        //float tmp = somme-f;
+        //float erreur= (f-(somme-tmp)) + (g-tmp);
+        //GrandFloat r= new GrandFloat(somme,erreur);
+        //return r;
+
+    //}
+
+
+
+
+
+
+
+
+    //Addition de deux grand float
     public static GrandFloat additionGrandFloat(GrandFloat ff , GrandFloat gg){
 	  GrandFloat tmp1tmp1,tmp2tmp2 ;
-	  tmp1tmp1 = additionPrecise(ff.f,gg.f);
-	  tmp2tmp2 = additionPrecise(ff.erreur,gg.erreur);
+	  tmp1tmp1 = additionSimple(ff.f,gg.f);
+	  tmp2tmp2 = additionSimple(ff.erreur,gg.erreur);
       // on ajoute la somme des erreurs à l'erreur de la somme
 	  tmp1tmp1.erreur+=tmp2tmp2.f;
 	  // on affine
@@ -47,5 +75,59 @@ public class GrandFloat {
 	  tmp1tmp1= additionSimple(tmp1tmp1.f,tmp1tmp1.erreur);
 	  return tmp1tmp1 ;
     }
+
+
+    public static float pow(float f, int n) {
+        if (n >= 0) {
+            if (n==0) return (float)1;
+            float tmp = pow(f,n/2);
+            if ( n%2 == 0 ) {
+                return tmp*tmp;
+            }
+            return f*tmp*tmp;
+        }
+        else {
+            if (n == -1){
+                return (float)1/f;
+            }
+            float tmp = pow(f,n/2);
+            if ( n%2 == 0 ) {
+                return tmp*tmp;
+            }
+            return tmp*tmp/f;
+        }
+
+    }
+
+
+    // transforme un float en Grand float, sans gain de précision( fonction donc auxiliaire ) mais aussi sans perte
+    public static GrandFloat split(float g, int n){
+        if ( n>22 || n<12){
+            throw new IllegalArgumentException(" le split doit être entre 12 et 22");
+        }
+        float split = ( pow(2,n)+1) * g;
+        float tmp = split - g;
+        float f = split - tmp;
+        float erreur = g-f;
+        return new GrandFloat(f,erreur);
+
+
+    }
+
+
+    // multiplie deux float et garde l'erreur dans notre structure
+    public static GrandFloat multiplicationSimple(float f, float g){
+        float produit = f*g;
+        GrandFloat tmp = split(f,12);
+        GrandFloat tmp1 = split(g,12);
+        // première erreur la différence
+        float erreur1 = produit - (tmp.f * tmp1.f);
+        // deuxième erreur : le produit de la partie haute de tmp par l'erreur de tmp1
+        float erreur2 = erreur1;
+        return tmp;
+
+    }
+
+
 
 }
--- src/main/resources/include/TestGrandFloat.java
+++ src/main/resources/include/TestGrandFloat.java
@@ -5,11 +5,16 @@ public class TestGrandFloat {
     public static void main(String[] args) {
         GrandFloat test = GrandFloat.additionSimple((float)3.123456723,(float)0.00234578123);
         System.out.println(test);
-        test = GrandFloat.additionPrecise((float)3.123456723,(float)0.00234578123);
+        test = GrandFloat.additionSimple((float)3.123456723,(float)0.00234578123);
         System.out.println(test);
-        GrandFloat pi = new GrandFloat((float)3.1415927,(float)-0.0000000464102068);
+        GrandFloat pi = new GrandFloat((float)3.141592,(float)0.000000653589793);
+        GrandFloat pisur4 = new GrandFloat((float)0.7853981,(float)0.00000006339);
         System.out.println("pi " +pi);
-        GrandFloat deuxpi= GrandFloat.additionGrandFloat(pi,pi);
-        System.out.println( " 2*pi " + deuxpi);
+        GrandFloat zebi= GrandFloat.additionGrandFloat(pi,pisur4);
+        System.out.println( " 5 pi / 4  " + zebi);
+       // GrandFloat troispi= GrandFloat.additionGrandFloat(zebi,pi);
+        //System.out.println( " 3*pi " + troispi);
+        //float troispis = 3*Mathe.pi();
+        //System.out.println("mauvais 3*pi = " + troispis);
     }
 }
