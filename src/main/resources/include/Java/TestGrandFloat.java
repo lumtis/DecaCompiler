d8ced39714e491b735580588aecd71b186142f59
Thami Benjelloun <thami.benjelloun@ensimag.grenoble-inp.fr>
2017-01-20 12:14:09 +0100

    pi moins f et adapt en GrandFLoat

--- src/main/resources/include/Java/TestGrandFloat.java
+++ src/main/resources/include/Java/TestGrandFloat.java
@@ -4,20 +4,30 @@
 public class TestGrandFloat {
     public static void main(String[] args) {
         GrandFloat test = GrandFloat.additionSimple((float)3.123456723,(float)0.00234578123);
-        System.out.println(test);
+        //System.out.println(test);
         test = GrandFloat.additionSimple((float)3.123456723,(float)0.00234578123);
-        System.out.println(test);
+        //System.out.println(test);
         GrandFloat pi = new GrandFloat((float)3.141592,(float)0.000000653589793);
         GrandFloat pisur4 = new GrandFloat((float)0.7853981,(float)0.00000006339);
-        System.out.println("pi " +pi);
+        //System.out.println("pi " +pi);
         GrandFloat zebi= GrandFloat.additionGrandFloat(pi,pisur4);
-        System.out.println( " 5 pi / 4  " + zebi);
+        //System.out.println( " 5 pi / 4  " + zebi);
        // GrandFloat troispi= GrandFloat.additionGrandFloat(zebi,pi);
         //System.out.println( " 3*pi " + troispi);
         //float troispis = 3*Mathe.pi();
         //System.out.println("mauvais 3*pi = " + troispis);
         GrandFloat r = GrandFloat.multiplicationSimple(2,(float)0.5);
-        System.out.println(r);
+        //System.out.println(r);
+        float test1 = Mathe.adapt((float)1000);
+        float test2= GrandFloat.adaptGranfloat((float)1000);
+        GrandFloat testm = new GrandFloat((float)6.2832,0);
+
+        System.out.println( "adapt de nizar             " + test1);
+        //System.out.println(" adpapt GrandFLoat          " + testm);
+        System.out.println(" adapt float par Grandfloat "+ test2);
+        System.out.println(" ce qu'il faut atteindre    " + 0.97353615844);
+        float g = GrandFloat.pimoinsf((float)3.141592);
+        System.out.println(g);
 
     }
 }
